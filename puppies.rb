cb661c9494c8392a0f3f51ffcbd21c3493998030
sans0987 <fairyfaye0226@gmail.com>
2018-07-30 14:48:19 +0000

    automatically backed up by learn

--- puppies.rb
+++ puppies.rb
@@ -7,7 +7,6 @@ def no_puppies(amount_of_puppies)
  end   
 end 
 
-no_puppies(1)
 no_puppies(0)
 
 # 2. Rachel's animal accumulation has been going well and we're going to cut her some slack. Write a new method called less_puppies that takes in a number of puppies as an argument and tells Rachel good job when she gets less than 3 puppies, to slow down if she's found exactly 3 puppies, and to get back on her animal restriction if she gets more than 3 puppies.
